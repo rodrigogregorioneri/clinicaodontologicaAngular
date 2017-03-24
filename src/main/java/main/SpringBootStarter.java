68d7a47eba8e3b7767147bba365f6f14fa4ee69f
Rodrigo Gregorio Neri <rodrigogregorioneri@hotmail.com>
2017-03-24 20:39:53 +0000

    commit inicial

---
+++ src/main/java/main/SpringBootStarter.java
@@ -0,0 +1,21 @@
+package main;
+
+
+/**
+ * Classe que representa ...
+ * 
+ * @author Rodrigo Gregorio Neri
+ * @version 1.0
+ * @since 2017-03-24
+ *
+ */
+ 
+public class SpringBootStarter {
+
+	/**
+	 * Construtor
+	 **/
+	public SpringBootStarter (){
+	}
+
+}
